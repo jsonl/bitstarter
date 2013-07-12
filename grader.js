e1917fc970b3e1a2fff45689fe22e1a38317aa65
Jason Liao <jasonliao360@gmail.com>
2013-07-12 01:11:49 +0000

    Added URL check in grader.js

--- grader.js
+++ grader.js
@@ -22,10 +22,12 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URLFILE_DEFAULT = "websource.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -35,6 +37,16 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+/*
+var downloadWebpage = function(url) {
+    console.log('Downloading from: ' + url);
+    rest.get(url).on('complete', function(data) {
+	var cache = data.toString();
+	fs.writeFileSync('websource.html', cache);
+    }); 
+};
+*/
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -65,8 +77,18 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Address of webpage')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    if(program.file) {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+    }
+    if(program.url) {
+        rest.get(program.url).on('complete', function(data) {
+	    var cache = data.toString();
+	    fs.writeFileSync('websource.html', cache);
+ 	    var checkJson = checkHtmlFile(URLFILE_DEFAULT, program.checks);
+        }); 
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
